c7ab8a4416601c28edf2b832798574b8292c90ea
armoro1 <pertaxa@gmail.com>
2020-02-11 09:51:56 +0100

    Avanzo en jornadas

--- public/js/scriptJornadas.js
+++ public/js/scriptJornadas.js
@@ -1,16 +1,15 @@
+
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('img1');
-var modalImg1 = document.getElementById("img1");
-var modalImg2 = document.getElementById("img2");
-var modalImg3 = document.getElementById("img3");
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -18,5 +17,5 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
