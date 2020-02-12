e68f1da6cf5e6bb53186c21a65ed92c30bc7ec10
KuroBuho <pablovidal1995@gmail.com>
2020-02-12 12:21:19 +0100

    Por fin funciona, falta bajar las imágenes y modificar el parámetro en maincontroller para cada imagen

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,7 +24,7 @@ class MainController extends AbstractController
         $user1 = $session->get('nombre_usuario');
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-
+            'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -35,6 +35,7 @@ class MainController extends AbstractController
     {
         //$user1 = $session->get('nombre_usuario');
         return $this->render('main/nuestrapropuesta.html.twig', [
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -54,7 +55,7 @@ class MainController extends AbstractController
             $entityManager->persist($contactoTo);
             $entityManager->flush();}
         return $this->render('main/contacto.html.twig', [
-          'imagenheader' => 'logo.png',
+          'imagenheader' => 'header-inicio.jpg',
             'form' => $form->CreateView()
         ]);
 
@@ -67,7 +68,7 @@ class MainController extends AbstractController
     {
         $user1 = $session->get('nombre_usuario');
         return $this->render('main/mirant.html.twig', [
-          'imagenheader' => 'logo.png'
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -78,7 +79,7 @@ class MainController extends AbstractController
     {
         $user1 = $session->get('nombre_usuario');
         return $this->render('main/jornadas.html.twig', [
-          'imagenheader' => 'logo.png'
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -89,7 +90,7 @@ class MainController extends AbstractController
     {
         //$user1 = $session->get('nombre_usuario');
         return $this->render('main/entidades.html.twig', [
-          'imagenheader' => 'logo.png'
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -99,7 +100,7 @@ class MainController extends AbstractController
     public function expandido()
     {
         return $this->render('main/leermas.html.twig', [
-          'imagenheader' => 'logo.png'
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
@@ -111,7 +112,7 @@ class MainController extends AbstractController
         $user1 = $session->get('nombre_usuario');
         $user1 = $session->get('nombre_usuario');
         return $this->render('main/forma_parte.html.twig', [
-          'imagenheader' => 'logo.png'
+          'imagenheader' => 'header-inicio.jpg'
         ]);
     }
 
