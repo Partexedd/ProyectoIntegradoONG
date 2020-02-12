b0c26b6acfe2a4be17ae151d50afee67b015aaca
robervip <vinesroberto13@gmail.com>
2020-02-12 09:27:45 +0100

    Boton en entidades

--- public/js/scriptEntidades.js
+++ public/js/scriptEntidades.js
@@ -1,21 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let imagesPopUp = document.getElementById('imagenPopUp');
-    let imagesPopUp2 = document.getElementById('imagenPopUp2');
-    let imagesPopUp3 = document.getElementById('imagenPopUp3');
+    let boton1PopUp = document.getElementById('boton1');
+    let boton1PopUp2 = document.getElementById('boton2');
+    let boton1PopUp3 = document.getElementById('boton3');
 
-    imagesPopUp.addEventListener('click', cancelarScroll);
-    imagesPopUp2.addEventListener('click', cancelarScroll);
-    imagesPopUp3.addEventListener('click', cancelarScroll);
-
-    let textoImagenPopUp = document.getElementById('desAcScroll');
-    let textoImagenPopUp2 = document.getElementById('desAcScroll2');
-    let textoImagenPopUp3 = document.getElementById('desAcScroll3');
-
-    textoImagenPopUp.addEventListener('click', cancelarScroll);
-    textoImagenPopUp2.addEventListener('click', cancelarScroll);
-    textoImagenPopUp3.addEventListener('click', cancelarScroll);
+    boton1PopUp.addEventListener('click', cancelarScroll);
+    boton1PopUp2.addEventListener('click', cancelarScroll);
+    boton1PopUp3.addEventListener('click', cancelarScroll);
 
     function cancelarScroll() {
         let body = document.getElementsByTagName("BODY")[0];
